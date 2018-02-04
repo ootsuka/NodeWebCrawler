9c6bfb6d5f51cac852bfcf3ed3527fd7ffbc2b01
xin shen <xinshen@xins-mbp.attlocal.net>
2018-02-04 15:51:26 -0600

    format data
    format data and presenting with chartjs

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -3,16 +3,7 @@ import { Bar, Line, Pie } from 'react-chartjs-2';
 
 class Chart extends Component {
   componentDidMount(){
-    this.callApi().then(function (result) {
-      const priceArr =
-      this.setState({
-        chartData: {
-          labels: result.map((item) => {
-            return
-          })
-        }
-      })
-    })
+    this.callApi();
   }
   callApi = async () => {
     const response = await fetch('http://localhost:3000/api/show');
@@ -20,52 +11,92 @@ class Chart extends Component {
     if (response.status != 200) {
       throw Error(body.message);
     }
-    return body;
+    var priceArr = [];
+    var indexArr = [];
+    var averagePriceArr = [];
+    var districtArr = [];
+    var map = new Map();
+
+    body.map((item) => {
+      let price = parseInt(item.price.match(/\d+/));
+      if (price) {
+        priceArr.push(price);
+      }
+      indexArr.push(item.name);
+      var district = item.where.substring(0,2);
+      if (typeof (map.get(district)) == 'undefined') {
+        if (price) {
+          map.set(district, {
+            count: 1,
+            averagePrice: price
+          });
+        }
+      } else {
+        if (price) {
+          var info = map.get(district);
+          var newCount = info.count + 1;
+          var newAverage = Math.round((info.averagePrice * info.count + price) / newCount);
+          map.set(district, {
+            count: newCount,
+            averagePrice: newAverage
+          });
+        }
+      }
+    });
+    map.forEach((value, key, map) => {
+      averagePriceArr.push(value.averagePrice);
+      districtArr.push(key);
+    });
+    console.log(map);
+    console.log(districtArr);
+    console.log(averagePriceArr);
+    this.setState({
+      chartData: {
+        labels: indexArr,
+          datasets: [{
+            label: 'price',
+            borderWidth: 1,
+            data: priceArr
+          }]
+      },
+      chartData2: {
+        labels: districtArr,
+          datasets: [{
+            label: 'average price',
+            borderWidth: 1,
+            data: averagePriceArr
+        }]
+      }
+    });
   }
   constructor(props){
     super(props);
     this.state = {
-      chartData: {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-            datasets: [{
-                label: 'Dataset 1',
-                borderWidth: 1,
-                data: [
-                    10,
-                    20,
-                    5,
-                    14,
-                    17,
-                    30,
-                    40
-                ]
-            }, {
-                label: 'Dataset 2',
-                borderWidth: 1,
-                data: [
-                    32,
-                    11,
-                    -9,
-                    8,
-                    25,
-                    49,
-                    -20
-                ]
-            }]
-      }
     }
   }
   render(){
     return(
+      <div>
       <div className="chart">
       <Bar
        data={this.state.chartData}
        width={100}
-       height={200}
+       height={400}
        options={{
-        maintainAspectRatio: false
+        maintainAspectRatio: false,
 }}
 />
+      </div>
+      <div className="chart">
+      <Bar
+       data={this.state.chartData2}
+       width={100}
+       height={400}
+       options={{
+        maintainAspectRatio: false,
+}}
+/>
+      </div>
       </div>
     )
   }
